4c70b3d08fcf37a69e4bee22149a14421c99527b
FinnTheFoxxo <finnoverflow@protonmail.com>
2023-12-02 13:08:27 -0500

    fixed reference list

--- fetch.py
+++ fetch.py
@@ -1,7 +1,5 @@
 from datetime import datetime
 import requests
-import json
-import time
 import glob
 import os
 
@@ -12,6 +10,7 @@ for y in x:
 def Fetch():
     lst = requests.get("https://www.tenable.com/cve/api/v1?sort=newest")
     sorting = []
+    ref = []
     for x in range(1, 49):
         cve = sorting.append(lst.json()['data']['hits'][x]['_id'][9:])
     sorting = list(map(int, sorting))
@@ -28,25 +27,27 @@ def Fetch():
             except KeyError:
                 base_score = "N/A"
             try:
-                ref = cve_url.json()['data']['_source']['references'][0]['url']
-            except KeyError:
-                ref = "N/A"
-            except IndexError:
-                ref = "N/A"
+                ref_list = cve_url.json()['data']['_source']['references']
+                for url in ref_list:
+                    ref.append(url['url'])
+            except (KeyError, IndexError):
+                ref = None
             score = cve_url.json()['data']['_source']['cvss3_severity']
             desc = cve_url.json()['data']['_source']['description']
-        if score == "Critical":
-            print(f"CVE-{datetime.now().year}-{y} - \033[0;91m{score} ({base_score})\033[0;0m \n {desc} \n References: {ref} \n")
-        elif score == "High":
-            print(f"CVE-{datetime.now().year}-{y} - \033[38;5;202m{score} ({base_score})\033[0;0m \n {desc} \n References: {ref} \n")
-        elif score == "Medium":
-            print(f"CVE-{datetime.now().year}-{y} - \033[0;93m{score} ({base_score})\033[0;0m \n {desc} \n References: {ref} \n")
-        elif score == "Low":
-            print(f"CVE-{datetime.now().year}-{y} - \033[38;5;82m{score} ({base_score})\033[0;0m \n {desc} \n References: {ref} \n")
+        if len(ref) == 0:
+            refs = "N/A"
         else:
-            print(f"CVE-{datetime.now().year}-{y} - {score} ({base_score}) \n {desc} \n References: {ref} \n")
-        io.write(f"CVE-{datetime.now().year}-{y} - {score} ({base_score}) \n {desc} \n References: {ref} \n\n")
-        io.flush()
+            refs = ''.join(ref); refs = refs.replace('http', ' http')
+        if score == "Critical":
+            print(f"CVE-{datetime.now().year}-{y} - \033[0;91m{score} ({base_score})\033[0;0m \n {desc} \n References: {refs} \n")
+        elif score == "High":
+            print(f"CVE-{datetime.now().year}-{y} - \033[38;5;202m{score} ({base_score})\033[0;0m \n {desc} \n References: {refs} \n")
+        elif score == "Medium":
+            print(f"CVE-{datetime.now().year}-{y} - \033[0;93m{score} ({base_score})\033[0;0m \n {desc} \n References: {refs} \n")
+        elif score == "Low":
+            print(f"CVE-{datetime.now().year}-{y} - \033[38;5;82m{score} ({base_score})\033[0;0m \n {desc} \n References: {refs} \n")
+        io.write(f"CVE-{datetime.now().year}-{y} - {score} ({base_score}) \n {desc} \n References: {refs} \n\n")
+        io.flush(); ref.clear()
 
 Fetch()
 
